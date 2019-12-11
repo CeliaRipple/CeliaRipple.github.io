c4a070df240636647c90d5dd4c7f4e1d18d6b74e
CeliaRipple <55159963+CeliaRipple@users.noreply.github.com>
2019-12-11 15:59:51 -0500

    Add files via upload

---
+++ qgis2web_2019_12_11-15_55_59_222336/data/startendpoints_2.js
@@ -0,0 +1,9 @@
+var json_startendpoints_2 = {
+"type": "FeatureCollection",
+"name": "startendpoints_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "id": "3710", "cnt": 3.0, "chk": 0.0, "ein": null, "eout": null, "location": "City Hall" }, "geometry": { "type": "Point", "coordinates": [ -75.1635657, 39.9515948 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "id": "2633", "cnt": 1.0, "chk": 1.0, "ein": null, "eout": null, "location": "Penn Museum" }, "geometry": { "type": "Point", "coordinates": [ -75.1924993, 39.9496467 ] } }
+]
+}
