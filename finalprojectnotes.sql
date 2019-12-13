d3259f562165b21009eb1064cbf804c4cbf06d0e
CeliaRipple <55159963+CeliaRipple@users.noreply.github.com>
2019-12-13 16:33:19 -0700

    Update finalprojectnotes.sql

--- finalprojectnotes.sql
+++ finalprojectnotes.sql
@@ -13,7 +13,7 @@ Alter table planet_OSM_roads Add column target integer
 ::create topology 
 SELECT pgr_createTopology('planet_osm_roads', 0.00001, 'way', 'osm_id')
 
-::analyze the topology. I do not know what was supposed to come out of this. 
+::analyze the topology. 
 SELECT pgr_analyzeGraph('planet_osm_roads', 0.000001, the_geom := 'way', id := 'osm_id')
 
 ::Dijkstra shortest distance only requires "target", "Source", "cost", and "id". Now calculate cost as the length 
@@ -33,10 +33,6 @@ SELECT * FROM pgr_dijkstra(
 	
 create view shortest 
 
-::visulize on the map does not work----
-select from planet_osm_roads
-where edge in (select edge from shortest)
-
 alter table planet_osm_roads add column walkingroute integer;
 update planet_osm_roads
 set walkingroute = edge
@@ -47,60 +43,12 @@ where osm_id = edge
 "walkingroute" is not null
 export selected features > walkingroute
 
-ALTER TABLE planet_osm_roads ADD COLUMN traveltime double precision;
-UPDATE planet_osm_roads SET traveltime = cost / 6000
-
-
-
-::: DAR ES SALAM
-:: create topology 
-
-Alter table roads Add column source integer;
-Alter table roads Add column target integer
-
-SELECT pgr_createTopology('roads', 0.001, 'geom', 'id')
-
-SELECT pgr_analyzeGraph('roads', 0.001, the_geom := 'geom', id := 'id')
-
-::cost as distance 
-ALTER TABLE roads ADD COLUMN cost double precision;
-UPDATE roads SET cost = st_length(geography(geom))
-
-:: weird extra step?
-CREATE INDEX source_idx ON roads(source);
-CREATE INDEX target_idx ON roads(target);
-CREATE INDEX geom_idx ON roads USING GIST(geom);
-
-
-ALTER TABLE roads ADD COLUMN traveltime double precision;
-UPDATE roads SET traveltime = cost / 6000
-
-:::osm line
+:::osm line transform to Nad 83
 change into NAD83 2272
 ALTER TABLE planet_osm_line
  ALTER COLUMN way TYPE geometry(linestring,2272) 
   USING st_transform(way,2272);
 
-Alter table validlines Add column source integer;
-Alter table validlines Add column target integer
 
-SELECT pgr_createTopology('planet_osm_line', 0.00001, 'way', 'osm_id')
-
-ALTER TABLE planet_osm_line ADD COLUMN cost double precision;
-UPDATE planet_osm_line SET cost = st_length(geography(way))
-
-
-Create table walkingroute2 as
-SELECT * FROM pgr_dijkstra(
-    'SELECT osm_id AS id, source, target, cost FROM planet_osm_roads',
-    2633, ARRAY[2350,3346,3710],
-    true)
-	
-	alter table planet_osm_roads add column walkingroute2 int
-	
-	update planet_osm_roads 
-set walkingroute2 = edge
-from walkingroute2
-where osm_id = edge
  
 
